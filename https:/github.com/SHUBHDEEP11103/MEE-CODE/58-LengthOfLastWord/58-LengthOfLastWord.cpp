1bcf62772d4af53fd7e8581ead987e39067c3b08
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-03 12:25:58 +0530

    Create 58-LengthOfLastWord

---
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/58-LengthOfLastWord/58-LengthOfLastWord.cpp
@@ -0,0 +1,15 @@
+// Last updated: 03/07/2026, 12:25:57
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int siz=s.size(),kount=0,flag=0;
+        for(int i=siz-1;i>=0;i--){
+            if(s[i]==' '&&flag)break;
+            if(s[i]!=' '){
+                flag=1;
+                kount++;
+            }
+        }
+        return kount;
+    }
+};
