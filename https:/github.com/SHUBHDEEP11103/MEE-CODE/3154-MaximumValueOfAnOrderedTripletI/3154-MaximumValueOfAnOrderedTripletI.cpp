f79d08bba807cb2873e76fd7b96d76b3d5fd6ef4
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-03 12:22:43 +0530

    Create 3154-MaximumValueOfAnOrderedTripletI

---
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/3154-MaximumValueOfAnOrderedTripletI/3154-MaximumValueOfAnOrderedTripletI.cpp
@@ -0,0 +1,17 @@
+// Last updated: 03/07/2026, 12:22:43
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+       long long maxprefix =nums[0];
+       long long diff =LLONG_MIN;
+      long long maxtriplets =0;
+      for(int i =1; i<n-1; i++){
+        maxprefix  = max(maxprefix, (long long)nums[i-1]);
+        diff  = max(diff,maxprefix-nums[i]);
+        maxtriplets = max(maxtriplets,diff*nums[i+1]);
+
+      }
+      return maxtriplets;
+    }
+};
