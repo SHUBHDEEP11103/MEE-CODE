be5c842aef3ec5f2bdd5ae900da1d977ec93dd01
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-03 12:22:28 +0530

    Create 3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct

---
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct/3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct.cpp
@@ -0,0 +1,15 @@
+// Last updated: 03/07/2026, 12:22:28
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (int i = n - 1; i >= 0; i--) {
+            if (mp.find(nums[i]) != mp.end()) {
+                return i / 3 + 1;
+            }
+            mp[nums[i]]++;
+        }
+        return 0;
+    }
+};
