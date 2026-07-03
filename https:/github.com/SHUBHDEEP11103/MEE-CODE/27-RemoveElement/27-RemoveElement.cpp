40fb4af1b5ab39d1ea68b16133e23226f3b9f06c
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-03 12:26:11 +0530

    Create 27-RemoveElement

---
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/27-RemoveElement/27-RemoveElement.cpp
@@ -0,0 +1,14 @@
+// Last updated: 03/07/2026, 12:26:10
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
