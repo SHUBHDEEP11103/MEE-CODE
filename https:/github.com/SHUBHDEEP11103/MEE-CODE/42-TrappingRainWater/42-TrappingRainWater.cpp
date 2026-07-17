ebb20675eb13f7a0b68f44faadcff5018d4c4434
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-17 21:45:59 +0530

    File updated at 17/07/2026, 21:45:58

--- https:/github.com/SHUBHDEEP11103/MEE-CODE/42-TrappingRainWater/42-TrappingRainWater.cpp
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -1,27 +1,27 @@
-// Last updated: 17/07/2026, 21:05:32
+// Last updated: 17/07/2026, 21:45:58
 1 class Solution {
 2public:
 3    int trap(vector<int>& arr) {
 4        int n = arr.size();
-5        vector<int> leftmax(n);
+5        // vector<int> leftmax(n);
 6        vector<int> rightmax(n);
 7        int total = 0;
 8        int maxi=0;
-9        for(int i=0; i<n; i++){
-10            leftmax[i] = max(maxi,arr[i]);
-11            maxi = leftmax[i];
-12        }
+9        // for(int i=0; i<n; i++){
+10        //     leftmax[i] = max(maxi,arr[i]);
+11        //     maxi = leftmax[i];
+12        // }
 13        maxi = 0;
 14        for(int i=n-1; i>0; i--){
 15            maxi = max(maxi,arr[i]);
 16            rightmax[i] = maxi;
 17        }
-18        for(int i=0; i<n; i++){
-19            cout<<total<< " ";
-20            if(arr[i]<leftmax[i] && arr[i]<rightmax[i]){
-21                total += min(leftmax[i],rightmax[i])-arr[i];
-22            }
-23            cout<<total<< endl;
+18        int leftmax = arr[0];
+19        for(int i=0; i<n; i++){
+20            leftmax = max(leftmax,arr[i]);
+21            if(arr[i]<leftmax && arr[i]<rightmax[i]){
+22                total += min(leftmax,rightmax[i])-arr[i];
+23            }
 24        }
 25        return total;
 26    }
