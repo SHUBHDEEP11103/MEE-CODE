5bba57e261cfa59eedd3d45980205d2223ff70dd
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-17 21:05:33 +0530

    Create 42-TrappingRainWater

---
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -0,0 +1,28 @@
+// Last updated: 17/07/2026, 21:05:32
+1 class Solution {
+2public:
+3    int trap(vector<int>& arr) {
+4        int n = arr.size();
+5        vector<int> leftmax(n);
+6        vector<int> rightmax(n);
+7        int total = 0;
+8        int maxi=0;
+9        for(int i=0; i<n; i++){
+10            leftmax[i] = max(maxi,arr[i]);
+11            maxi = leftmax[i];
+12        }
+13        maxi = 0;
+14        for(int i=n-1; i>0; i--){
+15            maxi = max(maxi,arr[i]);
+16            rightmax[i] = maxi;
+17        }
+18        for(int i=0; i<n; i++){
+19            cout<<total<< " ";
+20            if(arr[i]<leftmax[i] && arr[i]<rightmax[i]){
+21                total += min(leftmax[i],rightmax[i])-arr[i];
+22            }
+23            cout<<total<< endl;
+24        }
+25        return total;
+26    }
+27};
