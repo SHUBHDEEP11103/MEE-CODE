85f6cf3a56db3b79574b2c24fc10e8404fff715e
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-28 17:53:09 +0530

    File updated at 28/07/2026, 17:53:09

--- https:/github.com/SHUBHDEEP11103/MEE-CODE/26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -1,16 +1,19 @@
-// Last updated: 28/07/2026, 17:14:26
+// Last updated: 28/07/2026, 17:53:09
 1class Solution {
 2public:
 3    int removeDuplicates(vector<int>& nums) {
-4        set<int> ans;
-5        for(auto i : nums){
-6            ans.insert(i);
-7        }
-8        int i=0;
-9        for(auto it : ans){
-10            nums[i] = it;
-11            i++;
-12        }
-13        return i;
-14    }
-15};
+4        int n = nums.size();
+5        int q;
+6        int p = q = 0;
+7        int i=0;
+8        
+9        for(int q=0; q<n; q++){
+10            if(nums[p] != nums[q]){
+11                nums[i+1] = nums[q];
+12                p = q;
+13                i++;
+14            }
+15        }
+16        return i+1;
+17    }
+18};
