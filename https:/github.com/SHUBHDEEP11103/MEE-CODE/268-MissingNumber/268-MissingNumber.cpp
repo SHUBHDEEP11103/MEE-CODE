b7bfa6573e91634c4a9ac97c961aef3a31673742
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-29 10:50:44 +0530

    File updated at 29/07/2026, 10:50:44

--- https:/github.com/SHUBHDEEP11103/MEE-CODE/268-MissingNumber/268-MissingNumber.cpp
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/268-MissingNumber/268-MissingNumber.cpp
@@ -1,14 +1,20 @@
-// Last updated: 29/07/2026, 10:32:10
+// Last updated: 29/07/2026, 10:50:44
 1class Solution {
 2public:
 3    int missingNumber(vector<int>& nums) {
-4        int n = nums.size();
-5        int total=0;
-6        for(auto i:nums){
-7            total += i;
-8        }
-9        int sum = (n*(n+1))/2;
-10        
-11        return sum-total;
-12    }
-13};
+4
+5        int n = nums.size();
+6
+7        vector<int> hash(n+1,0);
+8
+9        for(auto i:nums){
+10            hash[i] = 1;
+11        }
+12        for(int i=0; i<=n; i++){
+13            if(hash[i]==0){
+14                return i;
+15            }
+16        }
+17        return -1;
+18    }
+19};
