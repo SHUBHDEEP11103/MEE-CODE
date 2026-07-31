81fd297eaa6f840190cfbbeef997e757497884f9
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11103@users.noreply.github.com>
2026-07-31 08:22:17 +0530

    File updated at 31/07/2026, 08:22:17

--- https:/github.com/SHUBHDEEP11103/MEE-CODE/3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
@@ -1,22 +1,20 @@
-// Last updated: 31/07/2026, 08:21:59
+// Last updated: 31/07/2026, 08:22:17
 1class Solution {
 2public:
 3    int minimumPushes(string word) {
 4        int n = word.size();
 5        int ans = 0;
-6        // if(n <= 8){
-7        //     return n;
-8        // }
-9        int count = 1;
-10        while(n>0){
-11            if(n<8){
-12                ans += n*count;
-13            }else{
-14                ans += 8 * count;
-15            }
-16            count ++;
-17            n -= 8;
-18        }
-19        return ans;
-20    }
-21};
+6
+7        int count = 1;
+8        while(n>0){
+9            if(n<8){
+10                ans += n*count;
+11            }else{
+12                ans += 8 * count;
+13            }
+14            count ++;
+15            n -= 8;
+16        }
+17        return ans;
+18    }
+19};
