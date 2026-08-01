6b7333e06d9c1de90d30cba0f5b21e185cbfef67
SHUBHAM KUMAR JHA <143588717+SHUBHDEEP11104@users.noreply.github.com>
2026-08-01 19:41:43 +0530

    Create 455-AssignCookies

---
+++ https:/github.com/SHUBHDEEP11103/MEE-CODE/455-AssignCookies/455-AssignCookies.cpp
@@ -0,0 +1,23 @@
+// Last updated: 01/08/2026, 19:41:41
+1class Solution {
+2public:
+3    int findContentChildren(vector<int>& g, vector<int>& s) {
+4        int n = s.size();
+5        sort(g.begin(), g.end());
+6        sort(s.begin(), s.end());
+7        int l=0;
+8        int r=0;
+9        int max = 0;
+10        while(l<n && r<g.size()){
+11            if(s[l]>=g[r]){
+12                max++;
+13                l++;
+14                r++;
+15            }
+16            else if(s[l]<g[r]){
+17                l++;
+18            }
+19        }
+20        return max;
+21    }
+22};
